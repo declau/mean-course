704725423e30e29d10a6395886627fbb99f8a9b8
claudianodenis <denis.claudiano@dilettaaplicativos.com.br>
2019-07-13 18:53:59 -0300

    adding backend

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('First response');
+});
+server.listen(process.env.PORT || 3000);
+console.log("server on");
