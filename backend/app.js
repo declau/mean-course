8e0f44f9207153914bfe9c92c1def8f4cf78fb52
claudianodenis <denis.claudiano@dilettaaplicativos.com.br>
2019-07-17 21:34:23 -0300

    http client and cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((rep, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", 
+    "Origin, X-Requested-with, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", 
+    "GET, POST, PATCH, DELETE, OPTIONS")
+    next();
+});
+
 app.use('/api/posts', (req, res, next) => {
     const posts = [
         {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+
 
 
 @NgModule({
@@ -27,7 +29,8 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
