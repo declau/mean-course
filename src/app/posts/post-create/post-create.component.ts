8d7d2899be09be1863505dd8c8a9237b521302b3
claudianodenis <denis.claudiano@dilettaaplicativos.com.br>
2019-07-31 22:19:55 -0300

    create the edit form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { PostsService } from '../posts.service';
+import { Post } from '../post.model';
+
 
 
 
@@ -11,19 +14,41 @@ import { PostsService } from '../posts.service';
     styleUrls: ['./post-create.component.scss']
 
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
 
     enteredTitle = '';
     enteredContent = '';
-    
-    constructor(public postsService: PostsService) { }
+    private mode = 'create';
+    private postId: string;
+    post: Post;
 
+    constructor(public postsService: PostsService, public router: ActivatedRoute) { }
 
-    onAddPost(form: NgForm) {
+    ngOnInit() {
+        this.router.paramMap.subscribe((paramMap: ParamMap) => {
+            if (paramMap.has('postId')) {
+                this.mode = 'edit';
+                this.postId = paramMap.get('postId');
+                this.postsService.getPost(this.postId).subscribe(postData => {
+                    this.post = {id: postData._id, title: postData.title, content: postData.content};
+                });
+            } else {
+                this.mode = 'create';
+                this.postId = null;
+            }
+        });
+    }
+
+    onSavePost(form: NgForm) {
         if (form.invalid) {
             return;
         }
-        this.postsService.addPost(form.value.title, form.value.content);
+        if (this.mode === 'create') {
+            this.postsService.addPost(form.value.title, form.value.content);
+
+        } else {
+            this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+        }
         form.resetForm();
     }
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -14,39 +14,56 @@ export class PostsService {
 
     getPosts() {
         this.http.get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
-        .pipe(map((postData) => {
-            return postData.posts.map(post => {
-                return {
-                    title: post.title,
-                    content: post.content,
-                    id: post._id
-                };
-            });
-        })).subscribe((transformedPosts) => {
-            this.posts = transformedPosts;
-            this.postUpdated.next([...this.posts]);
+            .pipe(map((postData) => {
+                return postData.posts.map(post => {
+                    return {
+                        title: post.title,
+                        content: post.content,
+                        id: post._id
+                    };
+                });
+            })).subscribe((transformedPosts) => {
+                this.posts = transformedPosts;
+                this.postUpdated.next([...this.posts]);
 
-        });
+            });
     }
 
     getPostUpdateListener() {
         return this.postUpdated.asObservable();
     }
 
+    getPost(id: string) {
+        return this.http.get<{ _id: string, title: string, content: string }>("http://localhost:3000/api/posts/" + id);
+    }
+
     addPost(title: string, content: string) {
         const post: Post = { id: null, title: title, content: content };
         this.http.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post).subscribe((responseData) => {
-            const id  = responseData.postId;
+            const id = responseData.postId;
             post.id = id;
             this.posts.push(post);
             this.postUpdated.next([...this.posts]);
         });
     }
 
+    updatePost(id: string, title: string, content: string) {
+        const post: Post = { id: id, title: title, content: content };
+        this.http.put("http://localhost:3000/api/posts/" + id, post)
+            .subscribe(response => {
+                console.log(response);
+                const updatedPosts = [...this.posts];
+                const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+                updatedPosts[oldPostIndex] = post;
+                this.posts = updatedPosts;
+                this.postUpdated.next([...this.posts]);
+            });
+    }
+
     deletePost(postId: string) {
         this.http.delete("http://localhost:3000/api/posts/" + postId)
             .subscribe(() => {
-                const updatedPosts = this.posts.filter(post => post.id !== postId );
+                const updatedPosts = this.posts.filter(post => post.id !== postId);
                 this.posts = updatedPosts;
                 this.postUpdated.next([...this.posts]);
                 console.log("deleted!!!!!");
